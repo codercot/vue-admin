a1d1a0611d6f7542b8b7be53a2a96f803340c50e
admin <3171482172@qq.com>
2023-05-05 10:15:40 +0800

    初始化项目

--- src/api/product/tradeMark.js
+++ src/api/product/tradeMark.js
@@ -1,4 +1,13 @@
 import service from '@/utils/request.js'
 
 // 品牌管理管理
-export const reqTradeMarkList = (page,limit)=>service({url:`/admin/product/baseTrademark/${page}/${limit}`,method:"get"})
+export const reqTradeMarkList = (page, limit) => service({ url: `/admin/product/baseTrademark/${page}/${limit}`, method: "get" })
+
+// 添加商品&&删除商品
+export const reqAddorUploadTradeMark = (trade) => {
+  if (trade.id) {
+    return service({ url: "" })
+  } else {
+    return service({ url: "/admin/product/baseTrademark/save", method: 'post', data: trade })
+  }
+}
